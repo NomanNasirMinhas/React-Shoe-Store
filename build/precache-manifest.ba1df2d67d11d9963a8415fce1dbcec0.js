7878b9871e91a1395e0a78743df166d037175167
Noman Nasir Minhas <sp17bsse010@gmail.com>
2020-09-13 21:53:23 +0500

    All Exceptions Handled

---
+++ build/precache-manifest.ba1df2d67d11d9963a8415fce1dbcec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40adb629e3530d4595f25be4402f4e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d4baf643c3071b60df",
+    "url": "/static/css/main.2ca96ff6.chunk.css"
+  },
+  {
+    "revision": "45452d7099cecaada7a9",
+    "url": "/static/js/2.9d16984c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9d16984c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d4baf643c3071b60df",
+    "url": "/static/js/main.0abafaa6.chunk.js"
+  },
+  {
+    "revision": "e27b5af4c66cae20f5a3",
+    "url": "/static/js/runtime-main.a42d6432.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
